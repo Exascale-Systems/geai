a4eaf14fdcc4f1ce0594187e3fb09847020ad88e
mmkaram <mahdykaram@outlook.com>
2026-02-13 22:06:54 -0800

    update shell.nix

--- shell.nix
+++ shell.nix
@@ -20,10 +20,16 @@ pkgs.mkShell {
     tensorboard
     pyvista
 
+    # Qt dependencies for visualization
+    pkgs.qt5.qtbase
+    pkgs.qt5.qtwayland
+    pkgs.qt5.qtx11extras
+
     # System dependencies for compiling/running pip wheels
     pkgs.stdenv.cc.cc.lib
     pkgs.libGL
     pkgs.zlib
+    pkgs.glib
   ];
 
   # Automatically create and enter a virtual environment
@@ -40,5 +46,15 @@ pkgs.mkShell {
     pkgs.stdenv.cc.cc.lib
     pkgs.libGL
     pkgs.zlib
+    pkgs.glib
+    pkgs.qt5.qtbase
   ];
+
+  shellHook = ''
+    export QT_QPA_PLATFORM_PLUGIN_PATH="${pkgs.qt5.qtbase}/lib/qt-5.15.18/plugins"
+    export QT_QPA_PLATFORM="offscreen"
+    export PYVISTA_OFF_SCREEN="true"
+    export DISPLAY=""
+    echo "Qt platform set to offscreen for headless rendering"
+  '';
 }
